305338c08f4f3dd41c52537dfcec335274c2ebb0
hurleychin <qinhulin@gmail.com>
2026-08-04 13:34:23 +0800

    chore(workbuddy): bump version to 0.0.1

--- workbuddy/main.go
+++ workbuddy/main.go
@@ -339,7 +339,7 @@ type registrationCapability struct {
 }
 
 // version is injected at build time via -ldflags "-X main.version=...".
-var version = "0.8.7"
+var version = "0.0.1"
 
 func wbRegistration() registration {
 	return registration{
